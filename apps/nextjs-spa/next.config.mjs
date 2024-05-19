efebdc31b0cc478d010cc90af12590e6ece49946
Thomas <the.beel.616@gmail.com>
2024-05-20 01:54:25 +0200

    UI in necxtjs-spa

--- apps/nextjs-spa/next.config.mjs
+++ apps/nextjs-spa/next.config.mjs
@@ -3,6 +3,12 @@ const nextConfig = {
   output: "export",
   skipTrailingSlashRedirect: true,
   distDir: "dist",
+
+  transpilePackages: ["@repo/ui"],
+
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
 
 export default nextConfig;
